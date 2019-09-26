63f31a4e216b791af836781f5dd829653d39a715
Telelichko <teleleichko@yandex.ru>
2019-09-26 22:23:14 +0700

    Commiting date: 26.09.2019. Add project

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Game
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game.Resource1.Background;
+            this.ClientSize = new System.Drawing.Size(714, 523);
+            this.Name = "FormGame";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormGame_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class FormGame : Form
+    {
+        public Bitmap Circle_cursor = Resource1.Circle_cursor,
+                      Circle_run = Resource1.Img_run;
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+
+        public FormGame()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+        }
+
+        private void FormGame_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X;
+            _targetPosition.Y += _direction.Y;
+
+            if (_targetPosition.X < 0 || _targetPosition.X > 500)
+            {
+                _targetPosition.X = 200;            
+            }
+
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _targetPosition.Y = 200; 
+            }
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+     
+            g.DrawImage(Circle_cursor, handlerRect);
+            g.DrawImage(Circle_run, targetRect);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+        
+    }
+}
